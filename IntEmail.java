e459c8570cdbc69f0b6fce6ead1e8da9b58bfedc
riddlework <82472185+riddlework@users.noreply.github.com>
2023-11-30 23:32:27 -0700

    stuff

---
+++ IntEmail.java
@@ -0,0 +1,10 @@
+public class IntEmail{
+public static void main(String[] args) {
+if(args.length != 2){
+System.out.println("There was an error encountered in delivering the contents of your email");
+System.out.println("(this means that there were too few arguments supplied)");
+return;}Integer Ian = Integer.valueOf(args[0]);
+Integer Iago = Integer.valueOf(args[1]);
+
+
+}
