339048a33461ac07f0e06e012f2aade5978f6edd
riddlework <82472185+riddlework@users.noreply.github.com>
2023-11-30 23:47:05 -0700

    int example email done

--- IntEmail.java
+++ IntEmail.java
@@ -7,14 +7,23 @@ return;}Integer Ian = Integer.valueOf(args[0]);
 Integer Iago = Integer.valueOf(args[1]);
 
 String Sarah = "Let's highlight the integer operators";System.out.print(Sarah);
+;System.out.print("\n");
 ;int Isaac = (Ian + Iago);System.out.print(Isaac);
+;System.out.print("\n");
 ;int Isabella = (Iago - Ian);System.out.print(Isabella);
+;System.out.print("\n");
 ;int Isadora = (Isabella * Isaac);System.out.print(Isadora);
+;System.out.print("\n");
 ;System.out.print((Isadora / Isabella));
+;System.out.print("\n");
 ;int Isak = (Isadora % Isabella);String Sandra = "Now let's highlight the comparison operators";System.out.print(Sandra);
+;System.out.print("\n");
 ;boolean Bill = ((Isaac < Ian));System.out.print(Bill);
+;System.out.print("\n");
 ;boolean Bob = ((Iago > Isadora));System.out.print(Bob);
+;System.out.print("\n");
 ;boolean Billy = ((Isabella == Iago));System.out.print(Billy);
+;System.out.print("\n");
 ;String Sophie = "The equality operator can also be used on strings and bools";System.out.print(((Bob == Bill)));
 ;
 }
